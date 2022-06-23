fc3e8527e5dcd1cc46ed06037a726768e24dc771
Jan Honsbrok <jan.honsbrok@gmail.com>
2022-06-23 16:27:42 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# OpenGL Starter
+
+Template to get started with OpenGL 
+
+## Building
+
+Tested with cmake 3.20.4 and Visual Studio 2019
+
+### Prerequisites
+You need to install the following:
+- Visual Studio 2019
+- CMake
+
+### Building & Project generation
+Generate a Visual Studio Project on Windows:
+
+```
+git clone --recursive https://github.com/Latios96/opengl-starter.git
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+```
+If this succeeds, you can open the generated Visual Studio project in Visual Studio. You can also use another IDE like Clion, just make sure to pass the `OptiX_ROOT_DIR` in the Cmake options.
+
+If you prefer to build on the command line, use this command: (As far as I tested, this works only with the Visual Studio generator, not with ninja)
+```
+cmake --build . --config Release -- /M:%NUMBER_OF_PROCESSORS%
+```
---
+++ main.cpp
@@ -0,0 +1,202 @@
+#define GLAD_GL_IMPLEMENTATION
+#include <glad/gl.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include "linmath.h"
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <iostream>
+#include <format>
+#include <vector>
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
+
+struct Vertex {
+    vec3 pos;
+};
+
+struct Mesh {
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+};
+
+static const char *vertex_shader_text =
+    R"(#version 330
+
+    uniform mat4 MVP;
+    in vec3 vCol;
+    in vec3 vPos;
+    out vec3 color;
+
+    void main(){
+        gl_Position = MVP * vec4(vPos, 1.0);
+        color = vCol;
+    })";
+
+static const char *fragment_shader_text =
+    R"(#version 330
+
+    in vec3 color;
+    out vec4 fragment;
+
+    void main(){
+        fragment = vec4(1.0, 0.0, 0.0, 1.0);
+    })";
+
+static void error_callback(int error, const char *description) {
+    std::cerr << std::format("Error: {}", description) << std::endl;
+}
+void GLAPIENTRY
+MessageCallback(GLenum source,
+                GLenum type,
+                GLuint id,
+                GLenum severity,
+                GLsizei length,
+                const GLchar *message,
+                const void *userParam) {
+    fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+            (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""),
+            type, severity, message);
+}
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+void read_obj(std::string objPath, Mesh &mesh) {
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+
+    std::string err;
+
+    bool success = tinyobj::LoadObj(&attrib, &shapes, &materials, &err, objPath.data());
+    if (!success) {
+        std::cout << "Error when reading obj file: " + err << std::endl;
+        return;
+    }
+
+    for(int i=0;i<attrib.vertices.size();i=i+3){
+        mesh.vertices.push_back({{attrib.vertices[i], attrib.vertices[i + 1], attrib.vertices[i + 2]}});
+    }
+    for(auto &shape: shapes){
+        for(auto &indice: shape.mesh.indices){
+            mesh.indices.push_back(indice.vertex_index);
+        }
+    }
+}
+
+GLuint compile_shader(const char * shaderCode, GLenum  shaderType){
+    const GLuint shader = glCreateShader(shaderType);
+    glShaderSource(shader, 1, &shaderCode, nullptr);
+    glCompileShader(shader);
+
+    GLint isCompiled = 0;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &isCompiled);
+    if(isCompiled == GL_FALSE)
+    {
+        GLint maxLength = 0;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
+        std::string infoLog;
+        infoLog.resize(maxLength);
+        glGetShaderInfoLog(shader, maxLength, &maxLength, &infoLog[0]);
+
+        glDeleteShader(shader);
+
+        std::cerr << std::format("Error when compiling {} shader: :\n{}", shaderType == GL_VERTEX_SHADER ? "vertex": "fragment", infoLog);
+        throw std::runtime_error("Error when compiling shader");
+    }
+
+    return shader;
+}
+
+int main() {
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(640, 480, "OpenGL Teapot", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+
+    glfwMakeContextCurrent(window);
+    gladLoadGL(glfwGetProcAddress);
+    glfwSwapInterval(1);
+
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(MessageCallback, nullptr);
+
+    Mesh mesh;
+    read_obj("..\\teapot.obj", mesh);
+
+    GLuint vertex_buffer;
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, mesh.vertices.size()*sizeof(Vertex), mesh.vertices.data(), GL_STATIC_DRAW);
+
+    GLuint indices_buffer;
+    glGenBuffers(1, &indices_buffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_buffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh.indices.size() * sizeof(unsigned int), mesh.indices.data(), GL_STATIC_DRAW);
+
+    const GLuint vertex_shader = compile_shader(vertex_shader_text, GL_VERTEX_SHADER);
+    const GLuint fragment_shader = compile_shader(fragment_shader_text, GL_FRAGMENT_SHADER);
+
+    const GLuint program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+
+    const GLint mvp_location = glGetUniformLocation(program, "MVP");
+    const GLint vpos_location = glGetAttribLocation(program, "vPos");
+
+    GLuint vertex_array;
+    glGenVertexArrays(1, &vertex_array);
+    glBindVertexArray(vertex_array);
+    glEnableVertexAttribArray(vpos_location);
+    glVertexAttribPointer(vpos_location, 3, GL_FLOAT, GL_FALSE,
+                          sizeof(Vertex), (void *) offsetof(Vertex, pos));
+
+    while (!glfwWindowShouldClose(window)) {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        const float ratio = width / (float) height;
+
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        mat4x4 m, p, mvp;
+        mat4x4_identity(m);
+        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        mat4x4_mul(mvp, p, m);
+
+        glUseProgram(program);
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat *) &mvp);
+
+        glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_buffer);
+        glDrawElements(GL_TRIANGLES, mesh.indices.size(), GL_UNSIGNED_INT, nullptr);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
+
+//! [code]
